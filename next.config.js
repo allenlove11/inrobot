7db37fbfdd4c6bdb9c5aa64859e4557a78a36a80
allenlove11 <allendaibj@gmail.com>
2024-09-14 21:12:56 +0800

    Fix build issue with axios

--- next.config.js
+++ next.config.js
@@ -4,6 +4,20 @@ const nextConfig = {
   images: {
     domains: ['localhost'], // 允许加载来自 localhost 的图片
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "upgrade-insecure-requests"
+          }
+        ]
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig;
+
